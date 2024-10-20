b59595c3d14fd38bbb10cd6d57d041e1ec6927d1
AlfiyaK4060 <khan.alfiya395@gmail.com>
2024-10-19 19:20:30 -0700

    Update RestaurantApp.java
    Changed customer name to 'Bob', added 1 dollar to all the items in the menu and updated the method from addMenuItemToOrder to addToOrder

--- src/RestaurantApp.java
+++ src/RestaurantApp.java
@@ -3,17 +3,19 @@
 public class RestaurantApp {
     public static void main(String[] args) {
         // Creating customer
-        Customer customer = new Customer("Alice");
+        Customer customer = new Customer("Bob"); //Changed name to Bob
         
         // Creating menu items
-        MenuItem pizza = new MenuItem("Pizza", 8.99);
-        MenuItem burger = new MenuItem("Burger", 5.49);
-        MenuItem soda = new MenuItem("Soda", 1.99);
+        //added $1 to all the items
+        MenuItem pizza = new MenuItem("Pizza", 9.99); 
+        MenuItem burger = new MenuItem("Burger", 6.49);
+        MenuItem soda = new MenuItem("Soda", 2.99);
         
         // Customer adds items to their order
-        customer.addMenuItemToOrder(pizza);  // Collaborating with MenuItem and Order
-        customer.addMenuItemToOrder(burger);
-        customer.addMenuItemToOrder(soda);
+        //changed method to addToOrder
+        customer.addToOrder(pizza);  // Collaborating with MenuItem and Order
+        customer.addToOrder(burger);
+        customer.addToOrder(soda);
         
         // Customer places the order
         customer.placeOrder();  // Collaborating with Order to show order summary
