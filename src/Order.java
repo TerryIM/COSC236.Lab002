f6eb9a92b7d2c6c32c1b93a7c804e271a7587232
TerryIM <161023245+TerryIM@users.noreply.github.com>
2024-10-21 08:15:09 -0700

    Update Order.java
    Fixed output

--- src/Order.java
+++ src/Order.java
@@ -8,7 +8,7 @@ public class Order {
     
     public void addItem(MenuItem item) {
         items.add(item);
-        System.out.println("Menu item " + item + " was added");
+        System.out.println("Menu item " + item.getName() + " was added");
     }
     
     public double calculateTotal() {
